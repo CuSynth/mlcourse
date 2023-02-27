b76c9c174e123d3ff50b5b92cb296b5ee6b443ab
- <ggyygg586@gmail.com>
2023-02-27 11:09:43 +0700

    refactored+solved

--- task 4/dataTransformer.py
+++ task 4/dataTransformer.py
@@ -1,41 +1,72 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 class DataTransformer:
     
     def __init__(self):
-        pass
+        self.encoder = LabelEncoder()
+
 
         
-    def fillnans(self, X: pd.DataFrame):
-        total = X.isnull().sum().sort_values(ascending=False)
-        X = X.drop((total[total > 1]).index, axis=1)
-        X = X.drop(X.loc[X['Electrical'].isnull()].index)
+    def fillnans(self, X: pd.DataFrame, train_set: bool=True):
+        # total = X.isnull().sum().sort_values(ascending=False)
+        # X = X.drop((total[total > 1]).index, axis=1)
+
+        X =X.drop(X.loc[X['Electrical'].isnull()].index)
+
+        todrop = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu',
+                  'LotFrontage', 
+                  'GarageYrBlt', 'GarageCond', 'GarageType', 'GarageFinish', 'GarageQual',
+                  'BsmtFinType2', 'BsmtExposure', 'BsmtQual', 'BsmtCond', 'BsmtFinType1', 
+                  'MasVnrArea', 'MasVnrType']
+
+        X = X.drop(columns=todrop, axis=1)
+
+
+
+        to_fill = [ 'MSZoning', 'BsmtHalfBath', 'Utilities',
+                   'Functional', 'BsmtFullBath', 'BsmtFinSF1', 
+                   'BsmtFinSF2', 'BsmtUnfSF', 'KitchenQual', 'TotalBsmtSF',
+                   'Exterior2nd', 'GarageCars', 'Exterior1st',
+                   'SaleType']
+        for elem in to_fill:
+            X[elem] = X[elem].fillna(X[elem].mode()[0])
+
+        zero_nan_cols = ['GarageArea']
+        X[zero_nan_cols] = X[zero_nan_cols].fillna(0)
 
         return X
                 
     def encode(self, X):
-        X = pd.get_dummies(X, drop_first=True)
+        object_candidates = list(X.dtypes[X.dtypes == "object"].index.values)
+        
+        for col in object_candidates:
+            X[col] = self.encoder.fit_transform(X[col])
+        
         return X
-    
-    def fit_transform(self, X, obj_to_num: bool=True, drop_ID: bool=True):
+
+    # def encode(self, X):
+    #     X = pd.get_dummies(X, drop_first=True)
+    #     return X
+        
+    def fit_transform(self, X, obj_to_num: bool=True, train_set: bool=True, drop_id: bool=True):
         X = self.fillnans(X)
 
-        X.sort_values(by = 'GrLivArea', ascending = False)[:2]
-        X = X.drop(X[X['Id'] == 1299].index)
-        X = X.drop(X[X['Id'] == 524].index)
+        if train_set:
+            X.sort_values(by = 'GrLivArea', ascending = False)[:2]
+            X = X.drop(X[X['Id'] == 1299].index)
+            X = X.drop(X[X['Id'] == 524].index)
 
-        X['SalePrice']= np.log1p(X['SalePrice'])
+            X['SalePrice']= np.log1p(X['SalePrice'])
+        
         X['GrLivArea'] = np.log1p(X['GrLivArea'])
-
         X['TotalBsmtSF'] = np.log1p(X['TotalBsmtSF'])
 
-        if drop_ID:
+        if drop_id:
             X = X.drop("Id", axis=1)
-
+            
         if obj_to_num:
             X = self.encode(X)
-            
         
         return X     
--- task 4/fit_util.py
+++ task 4/fit_util.py
@@ -5,17 +5,9 @@ from sklearn.metrics import mean_squared_error
 from dataTransformer import DataTransformer
 
 
-def load_data():
+def load_data() -> pd.DataFrame:
     train_df: pd.DataFrame = pd.read_csv("data/train.csv")
-
-    transformer = DataTransformer()
-    train_df = transformer.fit_transform(train_df)
-
-    target = np.log1p(train_df['SalePrice'])
-    train_df: pd.DataFrame = train_df.drop(columns='SalePrice', axis=1)
-
-    return train_df, target
-
+    return train_df
 
 
 def rmse(y, y_pred):
@@ -39,14 +31,13 @@ def submission(transformer, gs_model, create_sub_file: bool=False, obj_to_num: b
     validation = pd.read_csv("data/test.csv")
 
     val_ids = validation["Id"]
-    validation = validation.drop(columns=["Id"])
 
-    validation['GrLivArea'] = np.log1p(validation['GrLivArea'])
-    validation['TotalBsmtSF'] = np.log1p(validation['TotalBsmtSF'])
-    validation = pd.get_dummies(validation, drop_first=True)
-
+    validation = transformer.fit_transform(validation, obj_to_num=obj_to_num, train_set=False)
+    to_categorical(validation)
+ 
     sub_predictions = gs_model.predict(validation)
-    print("RMSE submission: " + str(rmse(sub_predictions, np.log1p(submissions["SalePrice"]))))
+
+    print("RMSLE submission: " + str(rmse((sub_predictions), np.log1p(submissions["SalePrice"]) )))
     
     if create_sub_file:
         d = {'Id': val_ids.to_numpy(), 'SalePrice':  np.expm1(sub_predictions)}
